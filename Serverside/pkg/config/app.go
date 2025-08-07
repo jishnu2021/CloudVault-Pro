54c990970df85303d0023e781ad7478a26e5577a
jishnu2021 <jishnughosh98@gmail.com>
2025-08-08 02:02:07 +0530

    add deployed database link

--- Serverside/pkg/config/app.go
+++ Serverside/pkg/config/app.go
@@ -5,19 +5,17 @@ import (
 	"gorm.io/driver/mysql"
 )
 
-var (
-	DB *gorm.DB
-)
+var DB *gorm.DB
 
-func ConnectDB(){
-	dsn := "root:Jishnu@2025@tcp(127.0.0.1:3306)/filesharedb?charset=utf8mb4&parseTime=True&loc=Local"
+func ConnectDB() {
+	dsn := "root:OqqvlUbxmyiwlYkzBfGmXocopsFjSbYf@tcp(trolley.proxy.rlwy.net:36587)/railway?charset=utf8mb4&parseTime=True&loc=Local"
+
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil{
+	if err != nil {
 		panic("Failed to connect to database: " + err.Error())
 	}
 }
-
 
 func GetDB() *gorm.DB {
 	return DB
