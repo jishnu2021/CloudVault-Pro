eef8f46e44671719eea5425a92174dc433620c5a
jishnu2021 <jishnughosh98@gmail.com>
2025-08-08 17:03:31 +0530

    add static.json and render.yaml for render

--- ClientSide/src/App.tsx
+++ ClientSide/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import NavbarPage from "./components/Navbar"
 import Home from "./pages/Home"
 import Dashboard from "./components/Dashboard"
--- ClientSide/vite.config.ts
+++ ClientSide/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss()
   ],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+  },
+  // This is the key configuration for handling client-side routing in production
+  base: '/',
 });
